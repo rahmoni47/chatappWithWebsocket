16ac303911aeb751f593e2365d2dbef5d4ce8d4a
Rahmoni <ghedjatirahmoni@gmail.com>
2026-01-07 22:56:05 +0100

    fixing some small errors for code to compile

--- src/main/java/com/h80/chatapp/config/DisconnectEventListener.java
+++ src/main/java/com/h80/chatapp/config/DisconnectEventListener.java
@@ -4,6 +4,7 @@ import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 import com.h80.chatapp.Enum.MessageType;
@@ -15,6 +16,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Component
 @Slf4j
+@EnableWebSocketMessageBroker
 public class DisconnectEventListener {
 
     private final SimpMessageSendingOperations messager;
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -37,10 +37,11 @@ function onConnected() {
     stompClient.subscribe('/topic/public', onMessageReceived);
 
     // Tell your username to the server
-    stompClient.send("/app/chat.addUser",
-        {},
-        JSON.stringify({sender: username, type: 'JOIN'})
-    )
+    stompClient.send(
+      "/app/chat.addUser",
+      {},
+      JSON.stringify({ sender: username, type: "Connected" })
+    );
 
     connectingElement.classList.add('hidden');
 }
@@ -56,9 +57,9 @@ function sendMessage(event) {
     var messageContent = messageInput.value.trim();
     if(messageContent && stompClient) {
         var chatMessage = {
-            sender: username,
-            content: messageInput.value,
-            type: 'CHAT'
+          sender: username,
+          content: messageInput.value,
+          type: "Chat",
         };
         stompClient.send("/app/chat.sendMessage", {}, JSON.stringify(chatMessage));
         messageInput.value = '';
@@ -72,26 +73,26 @@ function onMessageReceived(payload) {
 
     var messageElement = document.createElement('li');
 
-    if(message.type === 'JOIN') {
-        messageElement.classList.add('event-message');
-        message.content = message.sender + ' joined!';
-    } else if (message.type === 'LEAVE') {
-        messageElement.classList.add('event-message');
-        message.content = message.sender + ' left!';
+    if (message.type === "Connected") {
+      messageElement.classList.add("event-message");
+      message.content = message.sender + " joined!";
+    } else if (message.type === "Disconnected") {
+      messageElement.classList.add("event-message");
+      message.content = message.sender + " left!";
     } else {
-        messageElement.classList.add('chat-message');
+      messageElement.classList.add("chat-message");
 
-        var avatarElement = document.createElement('i');
-        var avatarText = document.createTextNode(message.sender[0]);
-        avatarElement.appendChild(avatarText);
-        avatarElement.style['background-color'] = getAvatarColor(message.sender);
+      var avatarElement = document.createElement("i");
+      var avatarText = document.createTextNode(message.sender[0]);
+      avatarElement.appendChild(avatarText);
+      avatarElement.style["background-color"] = getAvatarColor(message.sender);
 
-        messageElement.appendChild(avatarElement);
+      messageElement.appendChild(avatarElement);
 
-        var usernameElement = document.createElement('span');
-        var usernameText = document.createTextNode(message.sender);
-        usernameElement.appendChild(usernameText);
-        messageElement.appendChild(usernameElement);
+      var usernameElement = document.createElement("span");
+      var usernameText = document.createTextNode(message.sender);
+      usernameElement.appendChild(usernameText);
+      messageElement.appendChild(usernameElement);
     }
 
     var textElement = document.createElement('p');
