9cba31466138213dd70fa20b7ead65404626c9ed
Rahmoni <ghedjatirahmoni@gmail.com>
2026-01-07 21:27:55 +0100

    Creating endpoints for connecting with the wesocket

--- src/main/java/com/h80/chatapp/config/WebSocketConfig.java
+++ src/main/java/com/h80/chatapp/config/WebSocketConfig.java
@@ -1,7 +1,12 @@
 package com.h80.chatapp.config;
 
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws");
+    }
 }
