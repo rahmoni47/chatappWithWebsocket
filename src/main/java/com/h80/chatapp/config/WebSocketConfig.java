eac9ff71fc9e25c0773a4f04101e03e74952331d
Rahmoni <ghedjatirahmoni@gmail.com>
2026-01-07 21:32:08 +0100

    adding message broker configuration

--- src/main/java/com/h80/chatapp/config/WebSocketConfig.java
+++ src/main/java/com/h80/chatapp/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.h80.chatapp.config;
 
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
@@ -8,5 +9,13 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws");
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        //seting where the broadcast path sould be
+        registry.enableSimpleBroker("/topic");
+        //seting where the procces by the system path sould be
+        registry.setApplicationDestinationPrefixes("/app");
     }
 }
