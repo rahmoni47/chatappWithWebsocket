a9068938047f41153de3ce5033224808c6de1e35
Rahmoni <ghedjatirahmoni@gmail.com>
2026-01-07 21:26:51 +0100

    Creating Websocket Config Class

---
+++ src/main/java/com/h80/chatapp/config/WebSocketConfig.java
@@ -0,0 +1,7 @@
+package com.h80.chatapp.config;
+
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    
+}
